d67fb3340ede42c6f331f04454d6f89b8de2bdad
rickya51 <121059971+rickya51@users.noreply.github.com>
2022-12-22 21:17:38 +0000

    SQL style guide changes

--- customers.sql
+++ customers.sql
@@ -1,16 +1,15 @@
 select
-  c.id as customer_id
-  ,c.name
-  ,c.email
-  ,min(o.created_at) as first_order_at
-  ,count(o.id) as number_of_orders
-from
-  analytics-engineers-club.coffee_shop.orders as o
-  left join analytics-engineers-club.coffee_shop.customers as c on o.customer_id=c.id
+  customers.id as customer_id,
+  customers.name,
+  customers.email,
+  min(orders.created_at) as first_order_at,
+  count(orders.id) as number_of_orders
+from analytics-engineers-club.coffee_shop.orders as orders
+left join analytics-engineers-club.coffee_shop.customers as customers on orders.customer_id = customers.id
 group by
-    c.id
-  ,c.name
-  ,c.email
+  customers.id,
+  customers.name,
+  customers.email
 order by
-  min(o.created_at)
+  first_order_at
 limit 5
