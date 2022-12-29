9fa0522f947216b8cf8111489e0b3d58503b2782
rickya51 <121059971+rickya51@users.noreply.github.com>
2022-12-29 19:03:01 +0000

    Added sources for customers and orders

--- coffee_shop_ricky/models/customers.sql
+++ coffee_shop_ricky/models/customers.sql
@@ -8,8 +8,8 @@ select
   customers.email,
   min(orders.created_at) as first_order_at,
   count(orders.id) as number_of_orders
-from analytics-engineers-club.coffee_shop.orders as orders
-left join analytics-engineers-club.coffee_shop.customers as customers on orders.customer_id = customers.id
+from {{ source('coffee_shop', 'orders') }} as orders
+left join {{ source('coffee_shop', 'customers') }} as customers on orders.customer_id = customers.id
 group by
   customers.id,
   customers.name,
